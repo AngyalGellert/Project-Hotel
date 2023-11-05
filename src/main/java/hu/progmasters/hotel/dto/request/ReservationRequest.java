ed69e478484a6b07e8e74e06641174c5e3bcccbd
tib92 <tib921891@gmail.com>
2023-11-05 17:12:46 +0100

    ReservationRequest annotation repaired.

--- src/main/java/hu/progmasters/hotel/dto/request/ReservationRequest.java
+++ src/main/java/hu/progmasters/hotel/dto/request/ReservationRequest.java
@@ -2,18 +2,16 @@ package hu.progmasters.hotel.dto.request;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
 public class ReservationRequest {
 
-    @NotNull
+    @NotBlank
     private String guestName;
     @PastOrPresent
     private LocalDate startDate;
-    @PastOrPresent
+    @FutureOrPresent
     private LocalDate endDate;
 }
